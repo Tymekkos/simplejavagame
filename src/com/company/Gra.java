08a77bf51a63a5aa53aec0d80c39194b325bf9ba
Tymekkos <tymek.koson@gmail.com>
2021-08-19 13:47:06 +0200

    levels

--- src/com/company/Gra.java
+++ src/com/company/Gra.java
@@ -15,6 +15,7 @@ public class Gra extends JPanel implements KeyListener, ActionListener {
     private int speed = 20;
     private int ey = 10;
     private int eyspeed = 1;
+    public int level = 1;
     private Random liczba = new Random();
     private int enemyX1 = liczba.nextInt(6)*100;
     private int enemyX2 = liczba.nextInt(6)*100;
@@ -27,43 +28,90 @@ public class Gra extends JPanel implements KeyListener, ActionListener {
         addKeyListener(this);
     }
     public void paint(Graphics g){
-        g.setColor(Color.black);
-        g.fillRect(0,0,700,600);
-        //Gracz
-        g.setColor(Color.ORANGE);
-        g.fillRect(x,475,60,60);
-        //Wróg
-        g.setColor(Color.red);
-        g.fillRect(enemyX1,ey,50,50);
-        g.fillRect(enemyX2,ey,50,50);
-        g.fillRect(enemyX3,ey,50,50);
-        //punkty
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("serif", Font.BOLD, 20));
-        g.drawString("Punkty: " +punkty,570, 20);
+        if(level == 1){
+            g.setColor(Color.black);
+            g.fillRect(0,0,700,600);
+            //Gracz
+            g.setColor(Color.ORANGE);
+            g.fillRect(x,475,60,60);
+            //Wróg
+            g.setColor(Color.red);
+            g.fillRect(enemyX1,ey,50,50);
+            g.fillRect(enemyX2,ey,50,50);
+            //punkty
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Punkty: " +punkty,570, 20);
+            g.drawString("Level: " +level,570, 40);
+        }
+        if(level == 2){
+            g.setColor(Color.black);
+            g.fillRect(0,0,700,600);
+            //Gracz
+            g.setColor(Color.YELLOW);
+            g.fillRect(x,475,60,60);
+            //Wróg
+            g.setColor(Color.ORANGE);
+            g.fillRect(enemyX1,ey,52,52);
+            g.fillRect(enemyX2,ey,52,52);
+            g.fillRect(enemyX3,ey,52,52);
+            //punkty
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Punkty: " +punkty,570, 20);
+            g.drawString("Level: " +level,570, 40);
+        }
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        ey+=eyspeed;
-        System.out.println();
-        repaint();
-        if(ey >= 580){
-            punkty++;
-            eyspeed++;
-            ey = 5;
-            enemyX1 = liczba.nextInt(6)*100;
-            enemyX2 = liczba.nextInt(6)*100;
-            enemyX3 = liczba.nextInt(6)*100;
+        if(level == 1){
+            ey+=eyspeed;
+            System.out.println();
+            repaint();
+            if(ey >= 580){
+                punkty++;
+                eyspeed++;
+                ey = 5;
+                enemyX1 = liczba.nextInt(6)*100;
+                enemyX2 = liczba.nextInt(6)*100;
+            }
+            Rectangle gracz = new Rectangle(x,475,60,60);
+            Rectangle wrog1 = new Rectangle(enemyX1,ey,50,50);
+            Rectangle wrog2 = new Rectangle(enemyX2,ey,50,50);
+            //Rectangle wrog3 = new Rectangle(enemyX3,ey,50,50);
+            if(gracz.intersects(wrog1) || gracz.intersects(wrog2)){
+                time.stop();
+            }
         }
-        Rectangle gracz = new Rectangle(x,475,60,60);
-        Rectangle wrog1 = new Rectangle(enemyX1,ey,50,50);
-        Rectangle wrog2 = new Rectangle(enemyX2,ey,50,50);
-        Rectangle wrog3 = new Rectangle(enemyX3,ey,50,50);
-        if(gracz.intersects(wrog1) || gracz.intersects(wrog2)){
-            time.stop();
+        if (punkty == 5){
+            level++;
+        }
+        if (level == 2){
+            ey+=eyspeed;
+            System.out.println();
+            repaint();
+            if(ey >= 580){
+                punkty++;
+                eyspeed++;
+                ey = 8;
+                enemyX1 = liczba.nextInt(6)*100;
+                enemyX2 = liczba.nextInt(6)*100;
+                enemyX3 = liczba.nextInt(6)*100;
+            }
+            Rectangle gracz = new Rectangle(x,475,60,60);
+            Rectangle wrog1 = new Rectangle(enemyX1,ey,52,52);
+            Rectangle wrog2 = new Rectangle(enemyX2,ey,52,52);
+            Rectangle wrog3 = new Rectangle(enemyX3,ey,52,52);
+            if(gracz.intersects(wrog1) || gracz.intersects(wrog2) || gracz.intersects(wrog3)){
+                time.stop();
+            }
         }
     }
+
+
 
     @Override
     public void keyTyped(KeyEvent e) {
