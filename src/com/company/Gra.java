cd36e6a8a82f9b96e6d70f4a252f14978b377e8a
Tymekkos <tymek.koson@gmail.com>
2021-08-29 21:10:32 +0200

    menu screen (not really working tho)

--- src/com/company/Gra.java
+++ src/com/company/Gra.java
@@ -2,15 +2,12 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.util.Random;
 
 import static com.company.Statistics.*;
 
-public class Gra extends JPanel implements KeyListener, ActionListener {
+public class Gra extends JPanel implements KeyListener, ActionListener, MouseListener {
 
     public Gra(){
         time = new Timer(1,this);
@@ -187,8 +184,6 @@ public class Gra extends JPanel implements KeyListener, ActionListener {
         }
     }
 
-
-
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -229,5 +224,30 @@ public class Gra extends JPanel implements KeyListener, ActionListener {
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("u clicked the mouse");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("u presseed the mouse");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("u released the mouse");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("u are in the zone");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("u are out of the zone");
     }
 }
